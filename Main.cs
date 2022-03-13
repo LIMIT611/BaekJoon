02b95fb2a61689549465b8680cd4bd75add8cd91
LIMIT <minedreach@gmail.com>
2022-03-14 07:26:42 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,34 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int numOfSubject = int.Parse(Console.ReadLine());
+        string[] scoreOfsubjectInp = new string[numOfSubject];
+        double[] scoreOfsubject = new double[numOfSubject];
+
+        scoreOfsubjectInp = Console.ReadLine().Split();
+        
+        for (int i = 0; i < numOfSubject; i++)
+        {
+
+            scoreOfsubject[i] = double.Parse(scoreOfsubjectInp[i]);
+
+        }
+
+        Array.Sort(scoreOfsubject);
+
+        double maxOfScore = scoreOfsubject[numOfSubject - 1];
+        double sumOfscore = 0;
+
+        for (int i = 0; i < numOfSubject; i++)
+        {
+
+            scoreOfsubject[i] = scoreOfsubject[i] / maxOfScore * 100;
+                
+            sumOfscore += scoreOfsubject[i];
+                
+        }
+            
+        Console.WriteLine(sumOfscore / numOfSubject);
 
     }
 
