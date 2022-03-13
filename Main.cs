e452c6c33909040c49e1d06cb4d2f6b99ba6317b
LIMIT <minedreach@gmail.com>
2022-03-14 07:18:31 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -6,8 +6,24 @@ class BaekJun
 
     public static void Main(String[] args)
     {
+    
+        string input = Console.ReadLine();
 
-        Console.WriteLine("Hello World!");
+        int N = int.Parse(input);
+
+        int count = 1, num = 0;
+
+        num = ((N / 10) + (N % 10)) % 10 + 10 * (N % 10);
+
+        while (num != N) {
+
+            num = ((num / 10) + (num % 10)) % 10 + 10 * (num % 10);
+
+            count++;
+
+        }
+
+        Console.WriteLine(count);
 
     }
 
