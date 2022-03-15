48aec28158a025bc5db01935febefbeb9d30b767
LIMIT <minedreach@gmail.com>
2022-03-15 22:59:52 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -4,10 +4,52 @@
 class BaekJun
 {
 
+    static int SumOfEachDigit(int n)
+    {
+        
+        int result = n, temp = n;
+
+        while (temp >= 10)
+        {
+
+            result += temp % 10; 
+
+            temp /= 10;
+
+        }
+
+        result += temp;
+
+        return result;
+
+    }
+
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int[] NumsArray = new int[10000];
+
+        
+        for (int i = 0; i < NumsArray.Length; i++)
+        {
+
+            NumsArray[i] = i + 1;
+
+        }
+
+        for (int i = 1; i <= NumsArray.Length; i++)
+        {
+
+            if(SumOfEachDigit(i) <= 10000) NumsArray[SumOfEachDigit(i) - 1] = 0;
+
+        }
+
+        for (int i = 0; i < NumsArray.Length; i++)
+        {
+
+            if(NumsArray[i] != 0) Console.WriteLine($"{NumsArray[i]}");
+
+        }
 
     }
 
