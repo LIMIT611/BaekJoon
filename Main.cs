f9dc7f582979222bffce81f4cbd5a22a09a2c776
LIMIT <minedreach@gmail.com>
2022-03-16 22:23:04 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,27 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        String[] s = Console.ReadLine().Split();
+        int H = 0, M = 0, time = 0;
+
+        H = int.Parse(s[0]);
+        M = int.Parse(s[1]);
+
+
+        s[0] = Console.ReadLine();
+
+        time = int.Parse(s[0]);
+
+
+        M += time;
+
+        loop:
+
+        if (M >= 60) {H += 1; M -= 60; goto loop;}
+        if (H >= 24) {H -= 24; goto loop;}
+        
+
+        Console.WriteLine($"{H} {M}");
 
     }
 
