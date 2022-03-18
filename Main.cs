922f823014dabf54f708e8a9446707b479e7fc9f
LIMIT <minedreach@gmail.com>
2022-03-18 23:43:08 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,15 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string input = Console.ReadLine();
+
+        int testScore = int.Parse(input);
+
+        if (testScore >= 90) { Console.WriteLine("A"); }
+        else if (testScore >= 80) { Console.WriteLine("B"); }
+        else if (testScore >= 70) { Console.WriteLine("C"); }
+        else if (testScore >= 60) { Console.WriteLine("D"); }
+        else { Console.WriteLine("F"); }
 
     }
 
