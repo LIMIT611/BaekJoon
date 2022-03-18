caff62349210490439a9e057626978f3b5ca78f8
LIMIT <minedreach@gmail.com>
2022-03-18 23:28:46 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,8 +7,28 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string[] input = new string[2];
 
+        input[0] = Console.ReadLine();
+        input[1] = Console.ReadLine();
+
+        int X = int.Parse(input[0]);
+        int Y = int.Parse(input[1]);
+
+        if (X > 0) {
+                
+            if (Y > 0) { Console.WriteLine("1"); }
+            if (Y < 0) { Console.WriteLine("4"); }
+
+        }
+            
+        if (X < 0) {
+
+            if (Y > 0) { Console.WriteLine("2"); }
+            if (Y < 0) { Console.WriteLine("3"); }
+
+        }
+        
     }
 
 }
