414be574dc0ceb5d64664bb552bce1cad1272d69
LIMIT <minedreach@gmail.com>
2022-03-18 23:35:04 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,21 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string[] input = Console.ReadLine().Split();
+
+        int N = int.Parse(input[0]);
+        int X = int.Parse(input[1]);
+
+        input = Console.ReadLine().Split();
+
+        for (int i = 0; i < N; i++) {
+                
+            int[] A = new int[N];
+            A[i] = int.Parse(input[i]);
+
+            if (A[i] < X) Console.Write($"{A[i]} ");
+
+        }
 
     }
 
