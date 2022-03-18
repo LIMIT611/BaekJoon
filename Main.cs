5013bd41abe881c2fca4daf9cb49ba423b83127f
LIMIT <minedreach@gmail.com>
2022-03-18 23:39:48 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,16 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string[] input = Console.ReadLine().Split();
+
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+        int C = int.Parse(input[2]);
+            
+        Console.WriteLine((A + B) % C);
+        Console.WriteLine(((A % C) + (B % C)) % C);
+        Console.WriteLine((A * B) % C);
+        Console.WriteLine(((A % C) * (B % C)) % C); 
 
     }
 
