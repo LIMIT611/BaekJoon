5419c25bb74d67e533a44e6c0d99566586775e4a
LIMIT <minedreach@gmail.com>
2022-03-18 23:53:33 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,28 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        int C = int.Parse(Console.ReadLine());
+
+        int numsMultiply = A * B * C;
+        int[] numsCount = new int[10];
+
+        while (numsMultiply > 0)
+        {
+
+            numsCount[numsMultiply % 10]++;
+
+            numsMultiply /= 10;
+
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+
+            Console.WriteLine($"{numsCount[i]}");
+
+        }
 
     }
 
