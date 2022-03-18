8779d5faf0ae7adee125c9e21c6ae3d9db3e165d
LIMIT <minedreach@gmail.com>
2022-03-18 23:48:58 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,17 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string[] input = Console.ReadLine().Split();
+
+        int H = int.Parse(input[0]);
+        int M = int.Parse(input[1]);
+
+        if (H == 0 && M < 45) { H += 24; }    
+        if (M < 45) { H -= 1; M += 60; }
+
+        M -= 45;
+
+        Console.WriteLine("{0} {1}", H, M);
 
     }
 
