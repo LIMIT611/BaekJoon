36ab86f01d9fb241e77940be2de1a60d81859525
LIMIT <minedreach@gmail.com>
2022-03-18 23:47:13 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,40 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int testCase = int.Parse(Console.ReadLine());
+            
+            
+        for (int i = 0; i < testCase; i++)
+        {
+                
+            string[] input = Console.ReadLine().Split();
+            int studentNum = int.Parse(input[0]);
+            int[] inputNum = new int[studentNum + 1];
+            float sum = 0, average = 0, count = 0;
+
+            for (int j = 0; j < studentNum + 1; j++)
+            {
+
+                inputNum[j] = int.Parse(input[j]);
+
+                if (j >= 1) sum += inputNum[j];
+                
+            }
+
+            average = sum / (float)studentNum;
+
+            for (int j = 0; j < studentNum + 1; j++)
+            {
+
+                if (j >= 1 && inputNum[j] > average) count++;
+
+            }
+
+            float result = count / (float)studentNum * 100;
+
+            Console.WriteLine($"{result:F3}%");
+
+        } 
 
     }
 
