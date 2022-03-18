02248a263d35d4f15c27c8a0ea62eb88993a4b0c
LIMIT <minedreach@gmail.com>
2022-03-18 23:54:18 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,32 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int[] numsArray = new int[9];
+
+        for (int i = 0; i < 9; i++)
+        {
+
+            numsArray[i] = int.Parse(Console.ReadLine());
+
+        }
+
+        int maxValue = numsArray[0]; int numLocate = 1;
+
+        for (int i = 1; i < 9; i++)
+        {
+
+            if (numsArray[i] > maxValue)
+            {
+                
+                maxValue = numsArray[i];
+
+                numLocate = i + 1;
+
+            }
+                
+        }
+
+        Console.WriteLine($"{maxValue}\n{numLocate}");
 
     }
 
