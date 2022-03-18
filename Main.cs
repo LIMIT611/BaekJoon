2abc277c5b97903424a7b889c5eec12592edeb6f
LIMIT <minedreach@gmail.com>
2022-03-18 23:50:06 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,24 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string input;
+
+        input = Console.ReadLine();
+
+        int year = int.Parse(input);
+
+        if (year % 4 == 0)
+        {
+                
+            if (year % 100 != 0) Console.WriteLine("1");
+                
+            else if (year % 400 == 0) Console.WriteLine("1");
+
+            else Console.WriteLine("0");
+
+        }
+
+        else Console.WriteLine("0");
 
     }
 
