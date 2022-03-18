39c6af3c57b8aff4cbfe2e06c8ac21cc5e49afc4
LIMIT <minedreach@gmail.com>
2022-03-18 23:38:09 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,51 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int N = int.Parse(Console.ReadLine());
+
+        string[] input = Console.ReadLine().Split();
+
+        int[] num = new int[N];
+
+        for (int i = 0; i < N; i++) {
+
+            num[i] = int.Parse(input[i]);
+
+        }
+
+        Array.Sort(num);
+            
+        Console.WriteLine($"{num[0]} {num[N-1]}");
+
+        /*  Another Solution
+            
+            string input = Console.ReadLine();
+
+            int N = int.Parse(input);
+
+            string[] arrayInput = Console.ReadLine().Split();
+
+            int[] num = new int[N];
+
+            for (int i = 0; i < N; i++) {
+
+                num[i] = int.Parse(arrayInput[i]);
+
+            }
+
+            int max = num[0]; int min = num[0];
+
+            for (int i = 1; i < N; i++) {
+
+                if (num[i] > max) max = num[i];
+
+                if (num[i] < min) min = num[i];
+
+            }
+            
+            Console.WriteLine($"{min} {max}");
+
+        */
 
     }
 
