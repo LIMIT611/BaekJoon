e5a36284bc2ff9a6639814d2019b34b559f592fc
LIMIT <minedreach@gmail.com>
2022-03-18 23:48:17 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,45 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int maxCount = 10;
+        int divNum = 42;
+        int count = 0;
+        int[] remainder = new int[maxCount];
+
+        for (int i = 0; i < maxCount; i ++)
+        {
+
+            remainder[i] = int.Parse(Console.ReadLine()) % divNum;
+
+        }
+
+        bool[] isRemain = new bool[divNum];
+
+        for (int i = 0; i < maxCount; i ++)
+        {
+
+            for (int j = 0; j < divNum; j ++)
+            {
+                    
+                if (remainder[i] == j)
+                {
+                        
+                    isRemain[j] = true;
+
+                }
+
+            }
+
+        }
+
+        for (int i = 0; i < isRemain.Length; i ++)
+        {
+
+            if (isRemain[i]) count++;
+
+        }
+
+        Console.WriteLine(count);
 
     }
 
