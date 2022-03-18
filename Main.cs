8cb962df33013a5cd7440bfd8ab7d4bc0c6f7c9a
LIMIT <minedreach@gmail.com>
2022-03-18 23:35:41 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,15 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string[] input = Console.ReadLine().Split();
+            
+        int a = int.Parse(input[0]) + int.Parse(input[1]);
+        int b = int.Parse(input[0]) - int.Parse(input[1]);
+        int c = int.Parse(input[0]) * int.Parse(input[1]);
+        int d = int.Parse(input[0]) / int.Parse(input[1]);
+        int e = int.Parse(input[0]) - d * int.Parse(input[1]);
+
+        Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}", a, b, c, d, e);
 
     }
 
