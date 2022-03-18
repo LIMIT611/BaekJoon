98d4ea34e0ceb98651f1e17de41d7b3a9a11de3d
LIMIT <minedreach@gmail.com>
2022-03-18 23:16:55 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,17 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int N = int.Parse(Console.ReadLine()), sum = 0;
+        String eachOfDigit = Console.ReadLine();
+
+        for (int i = 0; i < eachOfDigit.Length; i++)
+        {
+
+            sum += int.Parse(eachOfDigit[i].ToString());
+
+        }
+
+        Console.WriteLine(sum);
 
     }
 
