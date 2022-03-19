bda559e56b67ad55f3eb709895c301ff3831b5d2
LIMIT <minedreach@gmail.com>
2022-03-20 00:08:05 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,40 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string s = Console.ReadLine();
+        int[] alphbtLocate = new int[26];
+
+        for (int i = 0; i < 26; i++)
+        {
+
+            alphbtLocate[i] = -1;
+            
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+
+            for (int j = 0; j < s.Length; j++)
+            {
+
+                if ((char)(i + 97) == s[j])
+                {
+
+                    alphbtLocate[i] = j;
+                    break;
+
+                }
+
+            }
+
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+
+            Console.Write($"{alphbtLocate[i]} ");
+
+        }
 
     }
 
