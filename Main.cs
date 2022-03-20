4a735dfa5484a5563c8ec668a40b2081853e900d
LIMIT <minedreach@gmail.com>
2022-03-20 23:49:53 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -1,13 +1,34 @@
 ﻿using System;
-
+using System.Text;
 
 class BaekJun
 {
 
+    static void StringRepeat()
+    {
+
+        String[] s = Console.ReadLine().Split();
+        
+        StringBuilder p = new StringBuilder();
+
+        for(int i = 0; i < s[1].Length; i++)
+        {
+
+            for (int j = 0; j < int.Parse(s[0]); j++) p.Append(s[1][i]);
+
+        }
+
+        Console.WriteLine(p);
+
+    }
+
+
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        String T = Console.ReadLine();
+
+        for (int i = 0; i < int.Parse(T); i++) StringRepeat();
 
     }
 
