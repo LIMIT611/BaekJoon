d27bd333fae72111ee06dd1229e686cecca98e6a
LIMIT <minedreach@gmail.com>
2022-03-22 23:20:13 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,30 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        String s  = Console.ReadLine();
+
+        bool isWord = false;
+
+        int wordCnt = 0;
+
+
+        for (int i = 0; i < s.Length; i++)
+        {
+
+            if ((int)s[i] != 32)
+            {
+                
+                if (isWord == false) wordCnt++;
+
+                isWord = true;
+
+            }
+            else if (isWord == true) isWord = false;
+
+        }
+
+
+        Console.WriteLine(wordCnt);
 
     }
 
