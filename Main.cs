e03c4ac8e5ba24416856e409ab1d128c5302c2f1
LIMIT <minedreach@gmail.com>
2022-03-22 22:52:30 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,32 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        String s = Console.ReadLine();
+
+        int[] alphabetCount = new int[26];
+        int temp = 0;
+        bool isOverlap = false;
+
+        for (int i = 0; i < 26; i++) alphabetCount[i] = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+
+            if ((int)s[i] < 97) temp = (int)s[i] - 65;
+            else temp = (int)s[i] - 97;
+
+            alphabetCount[temp] += 1; 
+
+        }
+
+
+        temp = 0;
+
+        for (int i = 0; i < 26; i++) if (alphabetCount[i] > alphabetCount[temp]) temp = i;
+        for (int i = 0; i < 26; i++) if (alphabetCount[i] == alphabetCount[temp] && temp != i) isOverlap = true;
+
+        if (isOverlap != true) Console.WriteLine((char)(temp + 65));
+        else Console.WriteLine("?");
 
     }
 
