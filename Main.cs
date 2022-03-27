55600d11c27152ed2ae88f211f58e73a6e3010a8
LIMIT <minedreach@gmail.com>
2022-03-27 23:21:08 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -3,11 +3,31 @@
 
 class BaekJun
 {
-
+    
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        String[] s = Console.ReadLine().Split();
+
+        long fixedCost = int.Parse(s[0]), variableCost = int.Parse(s[1]), saleCost = int.Parse(s[2]);
+        long salesCnt = 0;
+
+        bool isBreakEvenPoint = false;
+
+        if (variableCost < saleCost)
+        {
+            while (isBreakEvenPoint == false)
+            {
+                salesCnt++;
+                
+                if (fixedCost + variableCost * salesCnt < saleCost * salesCnt) isBreakEvenPoint = true;
+            }
+        }
+
+        else salesCnt = -1;
+
+
+        Console.WriteLine(salesCnt);
 
     }
 
