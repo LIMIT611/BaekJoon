5f0b4d805f50cb5e2c07b915412b7de8aea7b697
LIMIT <minedreach@gmail.com>
2022-03-28 22:53:00 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,20 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string s = Console.ReadLine();
+
+        int HexaNum = int.Parse(s), minMoveCnt = 1;
+
+        
+        while (HexaNum > 1)
+        {
+            HexaNum -= minMoveCnt * 6;
+
+            minMoveCnt++;
+        }
+        
+
+        Console.WriteLine(minMoveCnt);
 
     }
 
