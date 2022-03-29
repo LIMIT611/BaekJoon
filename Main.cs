84ae0f6fa034c61a71e8c666b599181872eaa1aa
LIMIT <minedreach@gmail.com>
2022-03-29 23:31:49 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,8 +7,23 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string s = Console.ReadLine();
+        
+        int Cnt = 1, X = int.Parse(s);
 
+
+        while (X > Cnt)
+        {
+            X -= Cnt;
+
+            Cnt++;
+        }
+        
+
+        if (Cnt % 2 == 0) Console.WriteLine($"{X}/{Cnt - X + 1}");
+
+        else Console.WriteLine($"{Cnt - X + 1}/{X}");
+        
     }
 
 }
