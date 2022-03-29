aa470f132d3e8aa65a867ff3d210c89730772130
LIMIT <minedreach@gmail.com>
2022-03-30 00:03:39 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,8 +7,17 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string[] s = Console.ReadLine().Split();
 
+        int A = int.Parse(s[0]), B = int.Parse(s[1]), V = int.Parse(s[2]), dayCnt;
+        
+        
+        dayCnt = (V - A)/ (A - B);
+
+        while (A * dayCnt - B * (dayCnt - 1) < V) dayCnt++;
+
+
+        Console.WriteLine(dayCnt);
     }
 
 }
