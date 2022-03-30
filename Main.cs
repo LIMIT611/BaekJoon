bd96946a64d65ed2eaf89d26850ba49a119dc258
LIMIT <minedreach@gmail.com>
2022-03-30 22:56:04 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -6,8 +6,27 @@ class BaekJun
 
     public static void Main(String[] args)
     {
+        
+        string testCase = Console.ReadLine();
+        string[] s = new string[3];
+        int H, W, N, roomNum;
 
-        Console.WriteLine("Hello World!");
+        for (int i = 0; i < int.Parse(testCase); i++)
+        {
+            s = Console.ReadLine().Split();
+
+            H = int.Parse(s[0]);
+            W = int.Parse(s[1]);
+            N = int.Parse(s[2]);
+
+            if (N % H == 0) roomNum = H * 100 + N / H;
+
+            else roomNum = N % H * 100 + (N / H + 1);
+
+            Console.WriteLine(roomNum);
+        }
+
+        
 
     }
 
