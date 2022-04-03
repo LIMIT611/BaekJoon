80a1cc1626fc6a1d82be3fe5ed8c7bcb72aa2db9
LIMIT <minedreach@gmail.com>
2022-04-03 23:42:10 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,32 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int testCase = int.Parse(Console.ReadLine()), temp;
+
+        string[] s = Console.ReadLine().Split();
+        
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            temp = int.Parse(s[i]);
+
+            if ((temp != 2 && temp % 2 == 0) || temp == 1) testCase--;
+
+            else
+            {
+                for (int j = 3; j < temp; j += 2)
+                {
+                    if (temp % j == 0)
+                    {
+                        testCase--;
+                        break;
+                    }
+                }
+            }
+        }
+
+        
+        Console.WriteLine(testCase);
 
     }
 
