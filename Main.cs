dd2a963bc5b4fee87e955e42627adb42de704dd4
LIMIT <minedreach@gmail.com>
2022-04-06 00:13:48 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Text;
 
 
 class BaekJun
@@ -7,8 +7,56 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int N = int.Parse(Console.ReadLine());
+
+        StringBuilder primeFactor = new StringBuilder();
+
+        
+        for (int i = 2; i * i <= N; i++)
+        {
+            while (N % i == 0)
+            {
+                primeFactor.Append($"{i}\n");
+
+                N /= i;
+            }
+        }
+
+        if (N > 1) Console.WriteLine(primeFactor + $"{N}");
+        
+        else Console.Write(primeFactor);
 
     }
 
 }
+
+
+/* 
+
+while (N > 1)
+{
+    if (N % primeNum == 0)
+    {
+        primeFactor.Append($"{primeNum}\n");
+                
+        N /= primeNum;
+    }
+
+    else
+    {
+        while (isPrime == false)
+        {
+            primeNum++;
+            isPrime = true;
+                
+            for (int i = 2; i * i <= primeNum; i++)
+            {
+                if (primeNum % i == 0) isPrime = false;
+            }
+        }
+    }
+
+    isPrime = false;
+}
+
+ */
