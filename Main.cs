d97d197d57eb71013d4586f8ac7c4cb770c3f4e2
LIMIT <minedreach@gmail.com>
2022-04-07 00:32:11 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,24 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string[] s = Console.ReadLine().Split();
+        int M = int.Parse(s[0]), N = int.Parse(s[1]);
+        bool[] isPrime = new bool[N + 1];
+        
+        for (int i = 2; i < N + 1; i++) isPrime[i] = true;
+
+        for (int i = 2; i < N + 1; i++)
+        {
+            if (isPrime[i] == true)
+            {
+                for (int j = 2 * i; j <= N; j += i) isPrime[j] = false;
+            }
+        }
+
+        for (int i = M; i < N + 1; i++)
+        {
+            if (isPrime[i] == true) Console.WriteLine(i);
+        }
 
     }
 
