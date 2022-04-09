5063f76e7e62c49c73a1fc345a44d1fe076291bf
LIMIT <minedreach@gmail.com>
2022-04-09 22:39:36 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,32 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int testCase = int.Parse(Console.ReadLine()), N, maxPrime = 2;
+        bool[] isPrime = new bool[10001];
+
+        for (int i = 2; i <= 10000; i++) isPrime[i] = true;
+
+        for (int i = 2; i <= 10000; i++)
+        {
+            if (isPrime[i] == true)
+            {
+                for (int j = 2; j * i <= 10000; j++) isPrime[j * i] = false;
+            }
+        }
+
+        while (testCase > 0)
+        {
+            N = int.Parse(Console.ReadLine());
+            
+            for (int i = 2; i <= N / 2; i++)
+            {
+                if (isPrime[i] && isPrime[N - i]) maxPrime = i;
+            }
+
+            Console.WriteLine($"{maxPrime} {N - maxPrime}");
+
+            testCase--;
+        }
 
     }
 
