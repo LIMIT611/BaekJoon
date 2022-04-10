34b6d7360603b3c529a23389fe6a8123c250a65e
LIMIT <minedreach@gmail.com>
2022-04-11 00:16:13 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,17 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string[] s = Console.ReadLine().Split();
+        
+        int x = int.Parse(s[0]), y = int.Parse(s[1]), w = int.Parse(s[2]), h = int.Parse(s[3]);
+
+        int minX = x, minY = y;
+        
+        if (x > w - x) minX = w - x;
+        if (y > h - y) minY = h - y;
+        
+        if (minX < minY) Console.WriteLine(minX);
+        else Console.WriteLine(minY);
 
     }
 
