9755e91b1ed8ceb68ca9f6f298aa11b39f016395
LIMIT <minedreach@gmail.com>
2022-04-16 00:13:06 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -4,10 +4,20 @@
 class BaekJun
 {
 
+    static int Fibonacci(int n)
+    {
+        int result = 0;
+        
+        if (n >= 2) result = Fibonacci(n - 1) + Fibonacci (n - 2);
+        if (n < 2) result = n;
+
+        return result;
+    }
+
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        Console.WriteLine(Fibonacci(int.Parse(Console.ReadLine())));
 
     }
 
