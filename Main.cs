6e9bcf0dbadef07e4bf1fa534907c063c6d32dcc
LIMIT <minedreach@gmail.com>
2022-04-15 23:56:13 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -4,10 +4,21 @@
 class BaekJun
 {
 
+    public static int Factorial(int n)
+    {
+        int result = 1;
+
+        if (n != 0) result *= n * Factorial(n - 1);
+        
+        return result;
+    }
+
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Factorial(N));
 
     }
 
