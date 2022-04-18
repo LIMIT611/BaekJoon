cfde066df408746417b2f7f679817892cdacad8c
LIMIT <minedreach@gmail.com>
2022-04-18 23:18:27 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -7,7 +7,31 @@ class BaekJun
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        string[] s = Console.ReadLine().Split();
+        string[] cardNums_input = Console.ReadLine().Split();
+        
+        int N = int.Parse(s[0]), M = int.Parse(s[1]);
+        int closeSum, sum;
+        int[] cardNums = new int[N];
+        
+        for (int i = 0; i < N; i++) cardNums[i] = int.Parse(cardNums_input[i]);
+        
+        closeSum = cardNums[0] + cardNums[1] + cardNums[2];
+
+        for (int i = 0; i < N - 2; i++)
+        {
+            for (int j = i + 1; j < N - 1; j++)
+            {
+                for (int k = j + 1; k < N; k++)
+                {
+                    sum = cardNums[i] + cardNums[j] + cardNums[k];
+                    
+                    if (Math.Abs(M - sum) < Math.Abs(M - closeSum) && sum <= M) closeSum = sum;
+                }
+            }
+        }
+
+        Console.WriteLine(closeSum);
 
     }
 
