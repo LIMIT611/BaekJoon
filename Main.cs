9629f44c73baeab7d1b199b7c3062d5b8878076f
LIMIT <minedreach@gmail.com>
2022-04-19 23:52:13 +0900

    Complete

--- Main.cs
+++ Main.cs
@@ -4,11 +4,36 @@
 class BaekJun
 {
 
+    static int SumOfSplitNum(int n)
+    {
+        int result = n;
+        
+        while (n > 0)
+        {
+            result += n % 10;
+
+            n /= 10;
+        }
+
+        return result;
+    }
+
     public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello World!");
+        int N = int.Parse(Console.ReadLine());
+        int constructorMin = 0;
 
+        for (int i = 1; i < N; i++)
+        {
+            if (SumOfSplitNum(i) == N)
+            {
+                constructorMin = i;
+                break;
+            }
+        }
+
+        Console.WriteLine(constructorMin);
     }
 
 }
